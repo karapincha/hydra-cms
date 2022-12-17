b0247f0bedf66993e74f6d153d802029df8bff37
Kasun Peiris <kasun@karapincha.io>
2022-12-17 23:16:17 +0530

    DO settings

---
+++ config/env/production/server.ts
@@ -0,0 +1,7 @@
+export default ({ env }) => ({
+  proxy: true,
+  url: env("APP_URL"), // replaces `host` and `port` properties in the development environment
+  app: {
+    keys: env.array("APP_KEYS"),
+  },
+});
